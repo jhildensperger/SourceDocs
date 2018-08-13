d1d086c83b451148ff3f623214b2afb508f52562
Jim Hildensperger <jim@thebrewery.io>
2018-08-14 01:02:31 +0200

    Add first functional test

--- Tests/SourceDocsTests/SourceDocsTests.swift
+++ Tests/SourceDocsTests/SourceDocsTests.swift
@@ -8,8 +8,40 @@
 import XCTest
 @testable import SourceDocsLib
 
-class SourceDocsTests: XCTestCase {
-    func testRunFunction() {
-        SourceDocsLib.run()
+let tmpOutputDirectory = "tmp/test/"
+
+func setCommandLineArguments(_ arguments: String...) {
+    CommandLine.arguments.removeLast(CommandLine.arguments.count - 1)
+    CommandLine.arguments.append(contentsOf: arguments)
+}
+
+class GenerateCommandTests: XCTestCase {
+    override func setUp() {
+        try? FileManager.default.removeItem(atPath: tmpOutputDirectory)
+        
+        XCTAssertFalse(FileManager.default.fileExists(atPath: tmpOutputDirectory))
     }
+    
+    func testGenerateOutput() {
+        let options = GenerateCommandOptions(spmModule: "SourceDocsDemo",
+                                             moduleName: nil,
+                                             outputDirectory: tmpOutputDirectory,
+                                             sourceDirectory: "../../../",
+                                             contentsFileName: "README",
+                                             accessLevelString: "public",
+                                             includeModuleNameInPath: false,
+                                             clean: false,
+                                             collapsibleBlocks: true,
+                                             tableOfContents: true,
+                                             xcodeArguments: [])
+        
+        let result = GenerateCommand().run(options)
+        
+        XCTAssert(FileManager.default.fileExists(atPath: tmpOutputDirectory))
+        XCTAssertNil(result.error)
+    }
+}
+
+class SourceDocsTests: XCTestCase {
+
 }
